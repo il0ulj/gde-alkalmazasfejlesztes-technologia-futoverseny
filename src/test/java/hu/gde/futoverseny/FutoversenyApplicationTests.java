0e161df5a606ead5718a1048f9e1eefce4b1e6bd
Péter Birkás <il0ulj@neptun.gde.hu>
2024-05-16 07:28:39 +0200

    Initial commit

---
+++ src/test/java/hu/gde/futoverseny/FutoversenyApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.gde.futoverseny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutoversenyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
